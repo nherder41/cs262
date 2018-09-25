415eb500c9587b2a37dfaee2eea8467d6dbfbdc8
nherder41 <nateherder@gmail.com>
2018-09-24 23:44:10 -0400

    homework1

---
+++ homework1/app/src/main/java/ndh7/edu/calvin/cs262/homework1/MainActivity.java
@@ -0,0 +1,18 @@
+package ndh7.edu.calvin.cs262.homework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+
+    }
+}
